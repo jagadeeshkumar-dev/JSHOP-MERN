45c04ee486c15002c3c5b364e65b3c1d56e3e975
K.Jagadeesh Kumar <kjagadeesh74@gmail.com>
2021-10-03 01:57:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,20 +13,24 @@ var orderRoute=require('./routes/orderRoute')
 
 
 app.use(bodyParser.json())
-const path=require('path')
+const path = require('path');
 app.use('/api/products/',productsRoute)
 app.use('/api/user/',userRoute)
 app.use('/api/order/',orderRoute) 
 
 
-if(process.env.NODE_ENV === 'production'){
+ if(process.env.NODE_ENV === 'production')
+    {
+        app.use('/' , express.static('jshop/build'))
 
-    app.use('/', express.static('jshop/build'))
+        app.get('*' , (req,res)=>{
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'jshop/build/index.html'))
-    })
-} 
+            res.sendFile(path.resolve(__dirname , 'jshop/build/index.html'))
+
+        })
+
+
+    }
 
 
 const port =process.env.PORT || 8000
